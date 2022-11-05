50124bee97f4a7607d4f747a6282da5aa1d1457f
Daniel Jolley <dcjolley12@gmail.com>
2022-11-05 15:46:11 -0600

    fix mod, add to and from to protos

--- server/main.go
+++ server/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-  "google.golang.org/grpc"
-  "google.golang.org/grpc/credentials/insecure"
+	"context"
+
+	pb "github.com/DJolley12/chat_app/protos"
+	// "google.golang.org/grpc"
+	// "google.golang.org/grpc/credentials/insecure"
 )
+
+type server struct {
+	pb.MessagerServer
+}
+
+func (s *server) SendMessage(ctx context.Context, in *pb.ChatMessage) (*pb.ReceivedMessage, error) {
+	return nil, nil
+}
 
 func main() {
 
