b4fea5f8e4280de8a9e7a82ba28934a3a27bd609
Daniel Jolley <dcjolley12@gmail.com>
2022-11-05 19:12:47 -0600

    small changes

--- client/main.go
+++ client/main.go
@@ -1,7 +1,41 @@
 package main
 
-import ()
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	pb "github.com/DJolley12/chat_app/protos"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+)
 
 func main() {
+	flag.Parse()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewChatClient(conn)
 
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SendMessage(ctx, &pb.ChatMessage{
+		FromName:    "Me",
+		ToName:      "Me",
+		MessageBody: "a message that is cool",
+		IsEncrypted: false,
+	})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetName())
 }
--- server/main.go
+++ server/main.go
@@ -2,20 +2,41 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
 
 	pb "github.com/DJolley12/chat_app/protos"
+	"google.golang.org/grpc"
 	// "google.golang.org/grpc"
 	// "google.golang.org/grpc/credentials/insecure"
 )
 
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
 type server struct {
-	pb.MessagerServer
+	pb.ChatServer
 }
 
 func (s *server) SendMessage(ctx context.Context, in *pb.ChatMessage) (*pb.ReceivedMessage, error) {
-	return nil, nil
+	log.Printf("received message from %v", in.GetFromName())
+	return &pb.ReceivedMessage{Name: in.GetFromName()}, nil
 }
 
 func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
 
+	s := grpc.NewServer()
+	pb.RegisterChatServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
